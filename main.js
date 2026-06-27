197122b91fa14d9655c2d76885c7ce5afdaa7d85
SrazerZ <srazerlockly@gmail.com>
2026-06-27 13:43:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+const endpoint = "http://10.113.31.226";
+
+function getDapurLed() {
+    fetch(endpoint + "/dapur", {
+        method: "GET"
+    }).then(response => response.text()).then(result => {
+        if(result == "ON") {
+            leddapur.style.backgroundColor = "#0c55b4";
+            dapurledimage.src="./assets/torch.png";
+        }else {
+            leddapur.style.backgroundColor = "#96bff5";
+            dapurledimage.src="./assets/torch (1).png";
+        }
+    });
+}
+
+function getKamarLed() {
+    fetch(endpoint + "/kamar", {
+        method: "GET"
+    }).then(response => response.text()).then(result => {
+        if(result == "ON") {
+            ledkamar.style.backgroundColor = "#0c55b4";
+            kamarledimage.src="./assets/torch.png";
+        }else {
+            ledkamar.style.backgroundColor = "#96bff5";
+            kamarledimage.src="./assets/torch (1).png";
+        }
+    });
+}
+
+function getRuangMakanLed() {
+    fetch(endpoint + "/ruangmakan", {
+        method: "GET"
+    }).then(response => response.text()).then(result => {
+        if(result == "ON") {
+            ledruangmakan.style.backgroundColor = "#0c55b4";
+            ruangmakanledimage.src="./assets/torch.png";
+        }else {
+            ledruangmakan.style.backgroundColor = "#96bff5";
+            ruangmakanledimage.src="./assets/torch (1).png";
+        }
+    });
+}
+
+
+
+function setDapurLed() {
+    fetch(endpoint + "/dapur", {
+        method: "POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+function setKamarLed() {
+    fetch(endpoint + "/kamar", {
+        method: "POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+function setRuangMakanLed() {
+    fetch(endpoint + "/ruangmakan", {
+        method: "POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+function setGarasiLed() {
+    fetch(endpoint + "/garasi", {
+        method: "POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+
+getDapurLed();
+getKamarLed();
+getRuangMakanLed();
